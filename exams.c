ff559fdf37da36333761c8ba86e34d87e831c7ab
Hamza Cherkaoui <hamzacherkaoui806@gmail.com>
2024-05-08 17:20:08 +0100

    emas

--- exams.c
+++ exams.c
@@ -155,18 +155,121 @@
 // int	main(int argc, char **argv)
 // {
 // 	if (argc > 1)
-// 		while (*argv[])
-// 			write(1, argv[2]++, 1);
+// 		while (*argv[1])
+// 			write(1, argv[1]++, 1);
 // 	write(1, "\n", 1);
 // 	return (0);
 // }
 
-int main(int argc, char **argv) {
-    if (argc > 1) {
-        while (*argv[argc - 2]) {
-            write(1, argv[argc - 2]++, 1);
+// int main(int argc, char **argv) {
+//     if (argc > 1) {
+//         while (*argv[argc - 2]) {
+//             write(1, argv[argc - 2]++, 1);
+//         }
+//     }
+//     write(1, "\n", 1);
+//     return 0;
+// }
+
+
+
+// void	ft_putchar(char c)
+// {
+// 	write(1, &c, 1);
+// }
+
+// int main() {
+//   int i = 'a';
+
+// 	while (i <= 'z')
+// 	{
+// 		if (i % 2 == 0)
+// 		{
+// 			ft_putchar(i - 32);
+// 		}
+// 		else 
+// 		{
+//       ft_putchar(i);
+//     }
+// 	i++;
+// 	}
+// 	ft_putchar('\n');
+// 	return (0);
+// }
+
+
+// void	ft_swap(int *a, int *b){
+// 	int temp;
+//   temp = *a;
+//   *a = *b;
+//   *b = temp;
+// }
+
+// int main() {
+// 	int a = 10;
+//   int b = 20;
+//   printf("a = %d, b = %d\n", a, b);
+//   ft_swap(&a, &b);
+//   printf("a = %d, b = %d\n", a, b);
+//   return (0);
+// }
+
+// int		ft_strcmp(char *s1, char *s2)
+// {
+// 	int i;
+
+// 	i = 0;
+// 	while (s1[i]  && s2[i]  && s1[i] == s2[i]){
+
+// 		i++;
+// 	}
+// 	return (s1[i] - s2[i]);
+// }
+
+// int main() {
+// 	char s1[] = "abcd";
+//   char s2[] = "abcb";
+//   printf( "%d", ft_strcmp(s1, s2));
+//   return 0;
+// }
+
+// int		ft_strcmp(char *s1, char *s2) {
+// 	int i = 0;
+
+// 	while (s1[i] == s2[i] && s1[i] && s2[i])
+// 	{
+// 		++i;
+// 	}
+// 	return s1[i] - s2[i];
+// }
+
+// int main() {
+// 	char s1[] = "abcd";
+//   char s2[] = "abcb";
+//   printf( "%d", ft_strcmp(s1, s2));
+//   return 0;
+// }
+
+
+#include <stdio.h>
+
+int count_letter_a( char* string) {
+    int count = 0;
+    int i = 0;
+
+    while (string[i]) {
+        if (string[i] == 'a') {
+            count++;
         }
+        i++;
     }
-    write(1, "\n", 1);
+    return count;
+}
+
+int main() {
+  char input_string[] = "Abracadabra";
+    int result = count_letter_a(input_string);
+    printf("Count: %d\n", result); 
+
     return 0;
 }
