18cc4b22148543b79217e316fc2681c2c2d88ebc
Hamza Cherkaoui <hamzacherkaoui806@gmail.com>
2024-05-10 01:36:25 +0100

    exmas

--- exams.c
+++ exams.c
@@ -340,11 +340,13 @@ void ft_putchar(char c) {
 
 
 // ! aff_first_param.c
+// #include <unistd.h>
+
 // int	main(int argc, char **argv)
 // {
 // 	if (argc > 1)
-// 		while (*argv[2])
-// 			write(1, argv[2]++, 1);
+// 		while (*argv[1 ])
+// 			write(1, argv[1]++, 1);
 // 	write(1, "\n", 1);
 // 	return (0);
 // }
@@ -365,24 +367,310 @@ void ft_putchar(char c) {
 
 // ! ft_strcpy
 
-char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
+// char	*ft_strcpy(char *dest, char *src)
+// {
+// 	int	i;
 
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+// 	i = 0;
+// 	while (src[i])
+// 	{
+// 		dest[i] = src[i];
+// 		i++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+
+// int main() {
+//   char dest[100];
+//   char src[100] = "Hello";
+//   ft_strcpy(dest, src);
+//   printf("%s\n", dest);
+//   return 0;
+// }
+
+
+// ! first_word.c
+
+// int main(int ac, char **av) {
+//   int i = 0;
+  
+//   if (ac == 2)
+//   {
+//     while (av[1][i] == ' ' || av[1][i] == '\t')  
+//       i++;
+//     while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t')
+//     {
+//       write(1, &av[1][i], 1);
+//       i++;
+//     }
+//   }
+//     write(1, "\n", 1);
+//     return 0;
+// }
+
+
+// ! ft_strlen.c
+// int		ft_strlen(char *str) {
+//   int i = 0;
+
+//   while (str[i])
+//     i++;
+//   return i;
+// }
+
+// int main() {
+//   char str[] = "hamza";
+//   printf("%d\n", ft_strlen(str));
+//   return 0;
+// }
+
+// ! swap.c
+
+// void  ft_swap(int *a, int *b) {
+//   int temp;
+//   temp = *a;
+//   *a = *b;
+//   *b = temp;
+// }
+
+// int main() {
+//   int a = 10;
+//   int b = 20;
+//   printf("a = %d, b = %d\n", a, b);
+//   ft_swap(&a, &b);
+//   printf("a = %d, b = %d\n", a, b);
+//   return 0;
+// }
+
+
+// ! rev_print.c
+
+// int		ft_strlen(char *str) {
+//   int i = 0;
+
+//   while (str[i])
+//     i++;
+//   return i;
+// }
+
+// char *ft_print(char *str) {
+//   int i = ft_strlen(str) - 1;
+//   while (i >= 0) {
+//   {
+//     write(1, &str[i], 1);
+//     i--;
+//   }
+//   write(1, "\n", 1);
+//   return (str);
+// }
+// }
+
+// int main() {
+//   char str[] = "hamza";
+//   ft_print(str);
+//   return 0;
+// }
+
+
+
+// int ft_strlen(char *str) {
+//   int i = 0;
+
+//   while (str[i])
+//     i++;
+//   return i;
+// }
+
+// char *ft_print(char *str) {
+//   int i;
+//   i = ft_strlen(str) - 1;
+//   // i--; // * => 'hamza' = 5 - 1 = 4
+//   while (i >= 0)
+//   {
+//     write(1, &str[i],1);
+//     i--;
+//   }
+//   write(1, "\n", 1);
+//   return str;
+// }
+
+// int main() {
+//   char str[] = "hamza";
+//   ft_print(str);
+//   return 0;
+// }
+
+
+
+// ! ft_strrev.c
+
+int		ft_strlen(char *str) {
+  int i = 0;
+
+  while (str[i])
+    i++;
+  return i;
 }
 
-int main() {
-  char dest[100];
-  char src[100] = "Hello";
-  ft_strcpy(dest, src);
-  printf("%s\n", dest);
-  return 0;
+// char	*ft_strrev(char *str)
+// {
+// 	int i;
+// 	int len;
+// 	char tmp;
+
+// 	i = 0;
+// 	len = ft_strlen(str) - 1;
+// 	while (len > i)
+// 	{
+// 		tmp = str[i];
+// 		str[i] = str[len];
+// 		str[len] = tmp;
+// 		i++;
+// 		len--;
+// 	}
+// 	return (str);
+// }
+
+// char *ft_strrev(char *str) {
+
+//   int length = 0;
+//   while (str[length]) {
+//     length++;
+//   }
+
+//   int start = 0;
+//   int end = length - 1;
+
+//   while (start < end) {
+//     char temp = str[start];
+//     str[start] = str[end];
+//     str[end] = temp;
+//     start++;
+//     end--;
+//   }
+
+//   return str;
+// }
+
+// int main() {
+//   char str[] = "hamza";
+//   printf("%s\n", ft_strrev(str));
+//   return 0;
+// }
+
+
+//! rot_13.c
+// void	rot13(char *str)
+// {
+// 	int	i = 0;
+
+// 	while( str[i])
+// 	{
+// 		if( (str[i] >= 'A' && str[i] <= 'M') || (str[i] >= 'a' && str[i] <= 'm') ){
+// 			str[i] += 13;
+//     }
+// 		else if( (str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n' && str[i] <= 'z') ){
+// 			str[i] -= 13;
+//     }
+// 		write(1, &str[i], 1);
+// 		i++;
+// 	}
+// }
+
+// int main() {
+//   char str[] = "az";
+//   rot13(str);
+//   return 0;
+// }
+
+// ! rotone.c
+// void	ft_rotone(char *str)
+// {
+// 	int	i = 0;
+
+// 	while( str[i])
+// 	{
+// 		if( (str[i] >= 65 && str[i] <= 89) || (str[i] >= 97 && str[i] <= 121) ){
+// 			str[i] += 1;
+//     }
+// 		else if( (str[i] == 122 || str[i] == 90))
+//     {
+// 			str[i] -= 25;
+//     }
+// 		write(1, &str[i], 1);
+// 		i++;
+// 	}
+// }
+
+
+// int main() {
+//   char str[] = "za"; 
+//   ft_rotone(str);
+//   return 0;
+// }
+
+// !last_word.c
+
+// void	last_word(char *str)
+// {
+// 	int	i = 0;
+// 	while (str[i])
+//   {
+// 		i++; // 17;
+//   }
+// 	i -= 1; // 16;
+// 	while(str[i] == '\t' || str[i] == 32)
+// 		i--;
+// 	while (i > 0)
+// 	{	
+//     // if(str[i] == 32)
+//     if(str[i] == 32 && str[i] == '\t')
+//     {
+// 			break;
+//     }
+// 		i--;
+// 	}
+// 	i++;
+// 	while (str[i] != '\0' && str[i] != 32 && str[i] != '\t')
+// 	{
+// 		write(1, &str[i], 1);
+// 		i++;
+// 	}
+// }
+// int main() {
+//   char str[] = "hamza simo  ilyass"; 
+//   last_word(str);
+//   return 0;
+// }
+
+
+// ! fizzbuzz.c
+void	ft_write_number(int number)
+{
+	if (number > 9)  // 1 > 9
+  {
+		ft_write_number(number / 10);  
+  } 
+	write(1, &"0123456789"[number % 10], 1); // 1 % 10 = 1
+}
+
+int	main(void)
+{
+	int	number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (number % 3 == 0)
+			write(1, "fizz", 4);
+		else if (number % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_write_number(number);
+		write(1, "\n", 1);
+		number++;
+	}
 }
