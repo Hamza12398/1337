922af66df369d4496cc9c61c7f79bc4ef9cdd941
Hamza Cherkaoui <hamzacherkaoui806@gmail.com>
2024-05-17 00:12:17 +0100

    odd.c

---
+++ EXAMS/odd.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+// *project name: odd.c
+
+// *create a program that only writes the character if the index its not on in is a multiple of 2.
+
+void even(char *str) 
+{
+    int index = 0;
+
+    while (*str != '\0') 
+    {
+        if (index % 2 != 0)
+        {
+            write(1, str, 1);
+        }
+        str++;
+        index++;
+    }
+    write(1, "\n", 1);
+}
+
+int main() 
+{
+    char *text = "hamzacherkaoui";
+    even(text);
+
+    return 0;
+}
